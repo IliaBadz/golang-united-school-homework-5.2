7c7c838a62dd56f889e39f102bffd23b064fdeac
IliaBadz <badzaghua.ilia@gmail.com>
2022-05-26 02:18:18 +0400

    add: solution to homework-5.2

--- cache.go
+++ cache.go
@@ -1,23 +1,49 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+type Value struct {
+	val      string
+	deadline time.Time
+}
 
 type Cache struct {
+	pair map[string]Value
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{make(map[string]Value)}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c Cache) Get(key string) (string, bool) {
 
+	if res, ok := c.pair[key]; ok {
+		if time.Now().Before(res.deadline) || res.deadline.IsZero() {
+			return res.val, true
+		} else {
+			delete(c.pair, key)
+		}
+	}
+
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.pair[key] = Value{val: value}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	res := make([]string, 0, len(c.pair))
+
+	for k := range c.pair {
+		res = append(res, k)
+	}
+
+	return res
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.pair[key] = Value{val: value, deadline: deadline}
 }
